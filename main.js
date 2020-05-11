0434abbb49a58c7709409627b4b67442f176f6f5
Eltion Musa <eltimusa4@gmail.com>
2020-05-11 17:37:02 +0200

    E

--- main.js
+++ main.js
@@ -6,15 +6,24 @@ function main() {
     getServerIp(function(url) {
         console.log("Url e serverit: ",url);
         server = url;
+        //search("Naruto")    
     });
 
-
+    
 
 
     $("#rec").on("click", function(){
-        search($("#search").val());
+        $('#animelist').empty();
+        search2($("#search").val());
     });
     
+
+    $("#search").keypress(function (e){
+        if(e.keyCode == 13){
+            $('#animelist').empty();
+            search2($("#search").val());
+        }
+    });
 
 
 //    getRecByAnime( "Naruto",function (data){
@@ -30,6 +39,24 @@ function search(key){
     });
 }
 
+function search1(key){
+    console.log("Kerko: ", key);
+    getRecByUserId( key, function (data){
+        console.log("Resultati: ", data)
+        addToList(data)
+    });
+}
+
+
+function search2(key){
+    console.log("Kerko: ", key);
+    getRecDes( key, function (data){
+        console.log("Resultati: ", data)
+        addToList(data)
+    });
+}
+
+
 function getServerIp( callback ){
     $.getJSON("serverUrl.php?url1", function(data){
         callback(data.url)
@@ -40,16 +67,30 @@ function getServerIp( callback ){
 function addToList(data){
     var e  = document.getElementById("animelist");
     data.forEach(element => {
-        var x = `<li> <img src="${element.img.replace("myanimelist.cdn-dena.com","cdn.myanimelist.net")}"> <span>${element.movie}</span> </li>`;
+        var x = `<li><div class="item"> <div><img src="${element.img.replace("myanimelist.cdn-dena.com","cdn.myanimelist.net")}"></div><div>${element.title }</div></div></li>`;
         e.innerHTML += x;
     });
 }
 
 function getRecByAnime( input, callback){
-    $.getJSON(server+"/anime?anime="+input, function (data, textStatus, jqXHR) {
+    console.log("Kerkese ne server: ",server+"/anime?anime="+input);
+    $.getJSON(server+"/anime?anime="+input, function (data) {
         callback(data)
-    }
-);
+    });
+}    
+
+function getRecByUserId( input, callback){
+    console.log("Kerkese ne server: ",server+"/model?id="+input);
+    $.getJSON(server+"/model?id="+input, function (data) {
+        callback(data)
+    });
+}
+
+function getRecDes( input, callback){
+    console.log("Kerkese ne server: ",server+"/tfidf?anime="+input);
+    $.getJSON(server+"/tfidf?anime="+input, function (data) {
+        callback(data)
+    });
 }
 
 window.onload = main;
